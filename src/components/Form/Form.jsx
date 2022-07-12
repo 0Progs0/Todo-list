c9710fb8f285e70f310596e6df2bdf90513bfee4
0Progs0 <zimenkov71rus.alex@mail.ru>
2022-07-12 18:15:59 +0300

    methods changes

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -15,6 +15,18 @@ const Form = () => {
 
     const [themes, setThemes] = useState(allThemes)
 
+    const addNewTopic = (e) => {
+        e.preventDefault()
+        const newTopic = topicValue
+        setThemes([...themes, newTopic])
+        setTopicValue('')
+    }
+
+    const removeTopic = (topic) => {
+        setThemes(themes.filter(t => t !== topic))
+        setTodosItems(todosItems.filter(t => t.topic !== topic))
+    }
+
     const addNewItem = (e) => {
         e.preventDefault()
         const newTodo = {
@@ -23,14 +35,19 @@ const Form = () => {
             todo:todoValue
         }
         setTodosItems([...todosItems, newTodo])
-        setThemes([...themes, newTodo.topic])
         items.push(newTodo)
+        setThemes(['All',...new Set(items.map(todo => todo.topic))])
         setTopicValue('')
         setTodoValue('')
     }
 
     const removeItem = (todo) => {
-        setTodosItems(todosItems.filter(t => t.id !==todo.id))
+        setTodosItems(todosItems.filter(t => t.id !== todo.id))
+        items.splice(items.indexOf(todo),1)
+        if (items.findIndex(t => t.topic === todo.topic) == -1) {
+            setThemes(themes.filter(t => t !== todo.topic))
+        }
+
     }
 
     const filterThemes = (theme) => {
